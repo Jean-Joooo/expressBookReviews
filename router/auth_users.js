01f4df2d67c81c545643cc69467d9a173d8239ee
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-10 05:21:28 +0100

    Update auth_users.js

--- router/auth_users.js
+++ router/auth_users.js
@@ -50,8 +50,13 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(300).json({message: "Review added"});
   });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+    return res.status(300).json({message: "Review deleted"});
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
